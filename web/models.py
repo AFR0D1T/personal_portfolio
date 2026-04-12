15b57385ef90a8952e5c289fc634979bed777c30
Bogdan <bpavlenko.pr@gmail.com>
2026-04-13 02:50:34 +0500

    Implemented model Proffile

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    profession = models.CharField(max_length=50, verbose_name='Профессия')
+    about = models.TextField(max_length=2000, verbose_name='Краткое описание')
+
+
