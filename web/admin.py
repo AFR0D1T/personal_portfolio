68fda60106270a7da72d88c3697af0617c1cf2d0
Bogdan <bpavlenko.pr@gmail.com>
2026-04-13 13:40:55 +0500

    registered models in the admin panel

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from web.models import Profile, SocialLinks, Education, Experience, Skills, Technologies
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(SocialLinks)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skills)
+admin.site.register(Technologies)
